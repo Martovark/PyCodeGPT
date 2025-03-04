32f2d1a734fd468c442bbf1260508be845e776d5
arkmartov <arkashamartov@gmail.com>
2025-03-04 17:07:03 +0300

    update

--- cert/pandas-numpy-eval/run.sh
+++ cert/pandas-numpy-eval/run.sh
@@ -5,4 +5,6 @@ source proj_env/bin/activate
 file_name=$1
 dump=$2
 
+echo "\n\nfile_name: $file_name, dump: $dump\n\n"
+
 python pandas_numpy_eval/evaluate_functional_correctness.py --sample_file $dump/cache/$file_name.jsonl --dump $dump
